346a62a657bf9a95b25826798773cb983bd8e191
Tim <Tim@DESKTOP-MOBTO9L>
2024-06-26 14:21:36 -0400

    Add project files.

---
+++ TestWebSite/Controllers/HSRController.cs
@@ -0,0 +1,46 @@
+﻿using Elfie.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace TestWebSite.Controllers
+{
+    public class HSRController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Physical()
+        {
+            return View(Physical); 
+        }
+        public IActionResult Fire()
+        {
+            return View();
+        }
+        public IActionResult Lightning()
+        {
+            return View();
+        }
+        public IActionResult Ice()
+        {
+            return View();
+        }
+        public IActionResult Wind()
+        {
+            return View();
+        }
+        public IActionResult Quantum()
+        {
+            return View();
+        }
+        public IActionResult Imaginary()
+        {
+            return View();
+        }
+        public IActionResult Hanya()
+        {
+            return View();
+        }
+    }
+}
